c5410dde2b95495645c99f50eba142ae8461ca27
Øyvind Kolbu <oyvind.kolbu@usit.uio.no>
2019-10-02 13:54:14 +0200

    Try without wheel.

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ def main():
                 'mreg-cli = mreg_cli.main:main',
             ],
         },
-        setup_requires=['wheel'],
         install_requires=install_requirements,
         packages=setuptools.find_packages(
             '.', include=('mreg_cli', 'mreg_cli.*')),
