2f2cd6c417017cb43a72f91b27bc88ffe69441ff
Terje Kvernes <terje@kvernes.no>
2024-04-23 15:12:37 +0200

    Add a run script for pyinstaller.

---
+++ run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.12
+# -*- coding: utf-8 -*-
+"""Run script for pyinstaller.
+
+Create a standalone executable as:
+
+pyinstaller --name mreg-cli run.py -F --hidden-import=mreg_cli.commands.host_submodules
+
+The finished binary will be in the `dist` directory.
+
+The hidden import is required to include the host submodules in the executable,
+due to using dynamic imports.
+"""
+import re
+import sys
+
+from mreg_cli.main import main
+
+if __name__ == "__main__":
+    sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
+    sys.exit(main())
