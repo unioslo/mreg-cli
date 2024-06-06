5dabdac6fd4395096d53c6a68c846be127787e44
Terje Kvernes <terjekv@users.noreply.github.com>
2024-06-06 16:41:42 +0200

    Support multiple destinations in host_copy. (#259)
    * Support multiple destinations in host_copy.
    
      - Also reduces output noise.
      - Also handles the situation where a destination already has the role in question.
      - Requires Role to be hashable.

--- mreg_cli/commands/policy.py
+++ mreg_cli/commands/policy.py
@@ -317,7 +317,7 @@ def host_add(args: argparse.Namespace) -> None:
     short_desc="Copy roles between hosts",
     flags=[
         Flag("source", description="Source host", metavar="SOURCE"),
-        Flag("destination", description="Destination host", metavar="DESTINATION"),
+        Flag("destination", description="Destination host", nargs="+", metavar="DESTINATION"),
     ],
 )
 def host_copy(args: argparse.Namespace) -> None:
@@ -326,16 +326,22 @@ def host_copy(args: argparse.Namespace) -> None:
     :param args: argparse.Namespace (source, destination)
     """
     source_name: str = args.source
-    destination_name: str = args.destination
-
     source = Host.get_by_any_means_or_raise(source_name)
-    destination = Host.get_by_any_means_or_raise(destination_name)
+    source_roles = set(source.roles())
 
-    for role in source.roles():
-        role.add_host(destination.name.hostname)
-        OutputManager().add_line(
-            f"Copied role {role.name} from {source_name} to {destination_name}"
-        )
+    for destination_name in args.destination:
+        destination = Host.get_by_any_means_or_raise(destination_name)
+        destination_roles = set(destination.roles())
+        OutputManager().add_line(f"Copying roles from from {source_name} to {destination_name}")
+
+        # Check if role already exists in destination
+        for role in source_roles & destination_roles:
+            OutputManager().add_line(f"    + {role.name} (existing membership)")
+
+        # Check what roles need to be added
+        for role in source_roles - destination_roles:
+            role.add_host(destination.name.hostname)
+            OutputManager().add_line(f"    + {role.name}")
 
 
 @command_registry.register_command(
