0ce75f4d9180e6e468711a33827f28d898bd342f
pederhan <pederhan@usit.uio.no>
2024-06-10 15:28:00 +0200

    Import host_submodules explicitly.

--- mreg_cli/commands/host_submodules/__init__.py
+++ mreg_cli/commands/host_submodules/__init__.py
@@ -11,3 +11,16 @@ command module, which is a bit hacky, but it works.
 Note: This design will imply circular imports, so the core host command
 imports the submodules after it has been initialized.
 """
+
+# We have to import each of the submodules explictly here in order to
+# ensure they are included when we build binaries with PyInstaller.
+# We also cannot do `from . import *`. Each module must be specified.
+from . import a_aaaa, bacnet, cname, core, rr
+
+__all__ = [
+    "a_aaaa",
+    "bacnet",
+    "cname",
+    "core",
+    "rr",
+]
