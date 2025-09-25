cc44325e634b95541ab26e8af0becd640ae5114f
pederhan <pederhan@usit.uio.no>
2025-09-25 11:22:25 +0200

    Remove site dir fallback (misunderstanding)
    It calls `os.path.expanduser`, not `pathlib.Path.expanduser`.

--- mreg_cli/dirs.py
+++ mreg_cli/dirs.py
@@ -10,37 +10,16 @@ import platformdirs
 
 logger = logging.getLogger(__name__)
 
-p = platformdirs.PlatformDirs(appname="mreg-cli", appauthor="UiO")
-
-# Path objects for various application directories
-
-try:
-    DATA_DIR = p.user_data_path
-    LOG_DIR = p.user_log_path
-    CACHE_DIR = p.user_cache_path
-    CONFIG_DIR = p.user_config_path
-except RuntimeError as e:
-    # This can happen if the user has no home directory
-    logger.error("Unable to determine user directories: %s", e)
-    # Fall back to site directories
-    DATA_DIR = p.site_data_path  # pyright: ignore[reportConstantRedefinition]
-    LOG_DIR = p.site_data_path / "logs"  # pyright: ignore[reportConstantRedefinition]
-    CACHE_DIR = p.site_cache_path  # pyright: ignore[reportConstantRedefinition]
-    CONFIG_DIR = p.site_config_path  # pyright: ignore[reportConstantRedefinition]
-
-APP_DIRS = (
-    DATA_DIR,
-    LOG_DIR,
-    CACHE_DIR,
-    CONFIG_DIR,
-)
+_pdirs = platformdirs.PlatformDirs(appname="mreg-cli", appauthor="UiO")
 
 
 # Default file names and paths
+CONFIG_DIR = _pdirs.user_config_path
 CONFIG_FILE_NAME = "mreg-cli.conf"
 CONFIG_FILE_DEFAULT = CONFIG_DIR / CONFIG_FILE_NAME
 """Default config file path."""
 
+LOG_DIR = _pdirs.user_log_path
 LOG_FILE_NAME = "mreg-cli.log"
 LOG_FILE_DEFAULT = LOG_DIR / LOG_FILE_NAME
 """Default log file path."""
@@ -53,6 +32,7 @@ DEFAULT_CONFIG_PATH: tuple[Path, ...] = tuple(
         # legacy locations
         # Unresolved to avoid issues if home dir is not available
         Path("~") / ".config" / CONFIG_FILE_NAME,
+        _pdirs.site_config_path / CONFIG_FILE_NAME,
         Path("/etc/mreg-cli.conf"),
         Path(sys.prefix) / "local" / "share" / CONFIG_FILE_NAME,
         Path(sys.prefix) / "share" / CONFIG_FILE_NAME,
