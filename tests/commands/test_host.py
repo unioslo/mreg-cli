8c4648e75a5ce551d4a397e026131cb259022349
pederhan <pederhan@usit.uio.no>
2025-12-03 13:59:11 +0100

    Improve override tests

--- tests/commands/test_host.py
+++ tests/commands/test_host.py
@@ -1,8 +1,11 @@
+import pytest
+
 from mreg_cli.commands.host_submodules.core import Override
+from mreg_cli.exceptions import InputFailure
 
 
 def test_override_members_are_strings() -> None:
-    """Ensure values are evaluated as strings, and can be compared with strings."""
+    """Ensure Override values are evaluated as strings, and can be compared with strings."""
     for member in Override:
         assert isinstance(member, str)
         assert isinstance(member.value, str)
@@ -10,7 +13,7 @@ def test_override_members_are_strings() -> None:
 
 
 def test_override_values() -> None:
-    """Ensure values are evaluated as strings, and can be compared with strings."""
+    """Ensure Override values are evaluated as strings, and can be compared with strings."""
     values = Override.values()
     for value in values:
         assert isinstance(value, str)
@@ -19,3 +22,26 @@ def test_override_values() -> None:
     assert "cname" in values
     assert Override.CNAME in values
     assert Override.CNAME.value in values
+
+
+def test_override_from_string() -> None:
+    """Ensure Override can be created from strings."""
+    for override in list(Override):
+        o = Override.from_string(override.value)
+        assert o == override
+
+        # Case insensitive
+        o_upper = Override.from_string(override.value.upper())
+        assert o_upper == override
+        o_lower = Override.from_string(override.value.lower())
+        assert o_lower == override
+
+        # Whitespace is stripped
+        o_whitespace = Override.from_string(f"  {override.value}  ")
+        assert o_whitespace == override
+
+    # Invalid values raise InputFailure
+    invalid_strings = ["invalid", "CNAME_", "ip", "", " "]
+    for invalid in invalid_strings:
+        with pytest.raises(InputFailure):
+            Override.from_string(invalid)
