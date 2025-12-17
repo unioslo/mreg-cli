c25845582ff2262069014dc85577f68a940d5eb1
pederhan <pederhan@usit.uio.no>
2025-12-17 10:44:48 +0100

    Add customizable test suite docker image + manual dispatch

--- ci/run_testsuite_and_record_V2.sh
+++ ci/run_testsuite_and_record_V2.sh
@@ -42,8 +42,18 @@ if [ ! -z "$1" ]; then
 fi
 echo "Python version $PYTHON_VERSION"
 
+# get mreg image from argument
+MREG_IMAGE="ghcr.io/unioslo/mreg:master"
+if [ ! -z "$2" ]; then
+	MREG_IMAGE=$2
+fi
+echo "MREG image $MREG_IMAGE"
+
 # build a container image for mreg-cli
 docker build -f Dockerfile -t mreg-cli --build-arg python_version=$PYTHON_VERSION ..
+
+# export image for docker-compose
+export MREG_IMAGE
 
 # start mreg+postgres in containers
 if [[ -n "$GITHUB_ACTIONS" ]]; then
